7c8511dbd62e528e50d3f607315f77be58ee0af6
Masoud Dev <masoud@masoud.dev>
2023-08-03 09:12:05 +0330

    Add images path

--- Task2_imagy/config/config.go
+++ Task2_imagy/config/config.go
@@ -7,15 +7,17 @@ import (
 )
 
 type Config struct {
-	HttpAddress string `json:"http_address"`
-	Port        string `json:"port"`
-	DbSchema    string `json:"db_schema"`
-	DbUsername  string `json:"db_username"`
-	DbPassword  string `json:"db_pass"`
-	DbName      string `json:"db_name"`
-	DbAddress   string `json:"db_address"`
-	DbPort      string `json:"db_port"`
-	DbSslMode   string `json:"db_ssl_mode"`
+	HttpAddress           string `json:"http_address"`
+	Port                  string `json:"port"`
+	DbSchema              string `json:"db_schema"`
+	DbUsername            string `json:"db_username"`
+	DbPassword            string `json:"db_pass"`
+	DbName                string `json:"db_name"`
+	DbAddress             string `json:"db_address"`
+	DbPort                string `json:"db_port"`
+	DbSslMode             string `json:"db_ssl_mode"`
+	RootDownloadPath      string `json:"root_download_path"`
+	UserContentUploadPath string `json:"user_content_upload_path"`
 }
 
 func Load(confPath string) (Config, error) {
