28ea46342798eb00869dd1c41c02ccfe5cbdb725
Masoud Dev <masoud@masoud.dev>
2023-08-03 21:35:57 +0330

    Change functions signature and error messages + import new dependency for db

--- Task2_imagy/adapter/store/setup.go
+++ Task2_imagy/adapter/store/setup.go
@@ -2,17 +2,19 @@ package store
 
 import (
 	"context"
-	"github.com/MasoudHeydari/Exercise_1/Task2_imagy/config"
-	"github.com/MasoudHeydari/Exercise_1/Task2_imagy/ent"
 	"log"
 	"net/url"
+
+	"github.com/MasoudHeydari/Exercise_1/Task2_imagy/config"
+	"github.com/MasoudHeydari/Exercise_1/Task2_imagy/ent"
+	_ "github.com/lib/pq"
 )
 
 type Database struct {
 	Client *ent.Client
 }
 
-func New(config config.Config) Database {
+func New(config config.Config) *Database {
 	u := url.URL{
 		Scheme:   config.DbSchema,
 		User:     url.UserPassword(config.DbUsername, config.DbPassword),
@@ -23,14 +25,14 @@ func New(config config.Config) Database {
 
 	client, err := ent.Open(u.Scheme, u.String())
 	if err != nil {
-		log.Fatal("Failed to open a connection to database", "error", err)
+		log.Fatal("Failed to open a connection to database ", "error", err)
 	}
 
 	if err = client.Schema.Create(context.Background()); err != nil {
-		log.Fatal("Failed to create database schema", "error", err)
+		log.Fatal("Failed to create database schema ", "error", err)
 	}
 
-	return Database{
+	return &Database{
 		Client: client,
 	}
 }
