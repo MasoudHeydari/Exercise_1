7ed9f47536c4f2aced4ae9b0caebf4a9167a51b5
Masoud Dev <masoud@masoud.dev>
2023-08-03 08:35:19 +0330

    Add config files

---
+++ Task2_imagy/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	HttpAddress string `json:"http_address"`
+	Port        string `json:"port"`
+	DbSchema    string `json:"db_schema"`
+	DbUsername  string `json:"db_username"`
+	DbPassword  string `json:"db_pass"`
+	DbName      string `json:"db_name"`
+	DbAddress   string `json:"db_address"`
+	DbPort      string `json:"db_port"`
+	DbSslMode   string `json:"db_ssl_mode"`
+}
+
+func Load(confPath string) (Config, error) {
+	confPath = filepath.Clean(confPath)
+	content, err := os.ReadFile(confPath)
+	if err != nil {
+		return Config{}, err
+	}
+	conf := new(Config)
+	err = json.Unmarshal(content, conf)
+	if err != nil {
+		return Config{}, err
+	}
+	return Config{}, nil
+}
