0e78cbe4cf591f9da6372540940ff4ca06da7353
Masoud Dev <masoud@masoud.dev>
2023-08-03 08:34:21 +0330

    Generate code for Image model

---
+++ Task2_imagy/ent/schema/image.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Image holds the schema definition for the Image entity.
+type Image struct {
+	ent.Schema
+}
+
+// Fields of the Image.
+func (Image) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int64("id").Unique().Immutable(),
+		field.String("original_url").NotEmpty(),
+		field.String("local_name").NotEmpty(),
+		field.String("file_extension").NotEmpty(),
+		field.Int64("file_size"),
+		field.Time("download_date").Default(time.Now),
+	}
+}
+
+// Edges of the Image.
+func (Image) Edges() []ent.Edge {
+	return nil
+}
