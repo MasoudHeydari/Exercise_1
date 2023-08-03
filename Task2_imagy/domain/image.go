b690e06f263fb6dfcad42f217a43ea861124afd9
Masoud Dev <masoud@masoud.dev>
2023-08-03 07:43:43 +0330

    Add json tagging

--- Task2_imagy/domain/image.go
+++ Task2_imagy/domain/image.go
@@ -3,10 +3,10 @@ package domain
 import "time"
 
 type Image struct {
-	ID            int64
-	OriginalURL   string
-	LocalName     string
-	FileExtension string
-	FileSize      int64
-	DownloadDate  time.Time
+	ID            int64     `json:"id"`
+	OriginalURL   string    `json:"original_url"`
+	LocalName     string    `json:"local_name"`
+	FileExtension string    `json:"file_extension"`
+	FileSize      int64     `json:"file_size"`
+	DownloadDate  time.Time `json:"download_date"`
 }
