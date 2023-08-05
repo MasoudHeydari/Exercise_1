19579aac7cb9710c43c2ab8b40cd5332e2c1e4eb
Masoud Dev <masoud@masoud.dev>
2023-08-06 01:14:34 +0330

    Add a func to remove comma delimeter from calculation lines

--- Task3_calculator/calculator/calculator.go
+++ Task3_calculator/calculator/calculator.go
@@ -16,6 +16,18 @@ func New(scn *bufio.Scanner) Calculator {
 	return Calculator{operands: []string{}, result: 0, scn: scn}
 }
 
+// rmComma remove comma separator from calculation lines.
+func rmComma(strs, fields []string) []string {
+	if len(strs) == 0 {
+		return fields
+	}
+	if strs[0] != "" {
+		s := strings.Split(strs[0], ",")
+		fields = append(fields, s...)
+	}
+	return rmComma(strs[1:], fields)
+}
+
 // rmSpaces remove space separator from calculation lines.
 func rmSpaces(strs, fields []string) []string {
 	if len(strs) == 0 {
