33e4c1b99584e330d4061ec23bfbb0cb781ad835
Masoud Dev <masoud@masoud.dev>
2023-08-06 01:37:19 +0330

    Create new Calculator instance and start it

--- Task3_calculator/main.go
+++ Task3_calculator/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
 
+	"github.com/MasoudHeydari/Exercise_1/Task2_calculator/calculator"
+)
+
+func main() {
+	scn := bufio.NewScanner(os.Stdin)
+	c := calc.New(scn)
+	c.StarCalculation()
 }
