bf6865436149135fb53e2d0d3fd1d796977c8afb
Masoud Dev <masoud@masoud.dev>
2023-08-06 01:11:50 +0330

    Add a func to remove space delimeter from calculation lines

--- Task3_calculator/calculator/calculator.go
+++ Task3_calculator/calculator/calculator.go
@@ -2,6 +2,7 @@ package calc
 
 import (
 	"bufio"
+	"strings"
 )
 
 type Calculator struct {
@@ -13,4 +14,14 @@ type Calculator struct {
 // New creates a Calculator
 func New(scn *bufio.Scanner) Calculator {
 	return Calculator{operands: []string{}, result: 0, scn: scn}
+}
+
+// rmSpaces remove space separator from calculation lines.
+func rmSpaces(strs, fields []string) []string {
+	if len(strs) == 0 {
+		return fields
+	}
+	s := strings.Fields(strs[0])
+	fields = append(fields, s...)
+	return rmSpaces(strs[1:], fields)
 }
